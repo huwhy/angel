be4bd4437b120a1923563cad5f753f21681c3f31
huwhy <huwhy@foxmail.com>
2017-04-11 16:24:31 +0800

    公众号事件处理

--- src/main/java/cn/huwhy/angel/web/MpEndpointController.java
+++ src/main/java/cn/huwhy/angel/web/MpEndpointController.java
@@ -69,8 +69,8 @@ public class MpEndpointController extends BaseController {
             Command command = wxBizMsgCryptBiz.transform(config, signature, timestamp, nonce, xmlMsg.toString());
             String replyMsg = eventHandler.handler(command);
             logger.debug("reply-xml: {}", replyMsg);
-            String encryptMsg = wxBizMsgCryptBiz.encryptMsg(config, replyMsg, timestamp, nonce);
-            printResponse(response, Strings.isNullOrEmpty(replyMsg) ? SUCCESS_MSG : encryptMsg);
+//            String encryptMsg = wxBizMsgCryptBiz.encryptMsg(config, replyMsg, timestamp, nonce);
+            printResponse(response, Strings.isNullOrEmpty(replyMsg) ? SUCCESS_MSG : replyMsg);
         } else {
             logger.debug("endpoint: {} - failure", id);
             printResponse(response, ERROR_MSG);
