95fff70461cfb6936f048fa48c376025682bc65d
huwhy <huwhy@foxmail.com>
2017-04-11 16:17:17 +0800

    公众号事件处理

--- src/main/java/cn/huwhy/angel/handler/TextMsgListener.java
+++ src/main/java/cn/huwhy/angel/handler/TextMsgListener.java
@@ -3,12 +3,17 @@ package cn.huwhy.angel.handler;
 import org.springframework.stereotype.Component;
 
 import cn.huwhy.wx.sdk.listener.Listener;
+import cn.huwhy.wx.sdk.message.Message;
+import cn.huwhy.wx.sdk.message.ReplyMsgBuilder;
+import cn.huwhy.wx.sdk.message.TextMessage;
 import cn.huwhy.wx.sdk.model.Command;
 
 @Component
 public class TextMsgListener extends Listener {
     @Override
     public String handle(Command command) {
-        return "http://angel.huwhy.cn/mp-article/5.html";
+        Message message = new TextMessage();
+        message.setContent("http://angel.huwhy.cn/mp-article/5.html");
+        return ReplyMsgBuilder.toXml(message);
     }
 }
