d204f97b21c599f4b13b686f92a83177bf431eca
huwhy <huwhy@foxmail.com>
2017-05-02 15:37:15 +0800

    博客

--- src/main/java/cn/huwhy/angel/dao/MyBatisConfig.java
+++ src/main/java/cn/huwhy/angel/dao/MyBatisConfig.java
@@ -23,6 +23,7 @@ import cn.huwhy.angel.po.Category;
 import cn.huwhy.angel.po.MpArticle;
 import cn.huwhy.angel.po.MpConfig;
 import cn.huwhy.angel.po.Param;
+import cn.huwhy.angel.po.Seo;
 import cn.huwhy.angel.po.User;
 
 /**
@@ -64,7 +65,7 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
         bean.setTypeAliasesPackage("cn.huwhy.angel.po");
         bean.setTypeAliases(new Class[]{
                 User.class, Category.class, Article.class,
-                MpArticle.class, MpConfig.class, Param.class
+                MpArticle.class, MpConfig.class, Param.class, Seo.class
         });
         bean.setConfigLocation(new ClassPathResource("cn/huwhy/angel/dao/sqlmap/sqlmap.xml"));
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
