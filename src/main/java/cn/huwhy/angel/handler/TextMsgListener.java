c835a8d454e6504e9e51a50e8eb9e20b8ff0de38
huwhy <huwhy@foxmail.com>
2017-05-06 17:58:41 +0800

    实时显示

--- src/main/java/cn/huwhy/angel/handler/TextMsgListener.java
+++ src/main/java/cn/huwhy/angel/handler/TextMsgListener.java
@@ -1,7 +1,11 @@
 package cn.huwhy.angel.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
 
 import cn.huwhy.angel.biz.manager.MpReplyManager;
 import cn.huwhy.angel.po.MpReply;
@@ -16,11 +20,14 @@ import static cn.huwhy.wx.sdk.model.Command.EVENT_KEYS.TEXT_MSG;
 @Component
 public class TextMsgListener extends Listener {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private MpReplyManager mpReplyManager;
 
     @Override
     public String handle(Command command) {
+        logger.info("command: {}", JSON.toJSONString(command));
         Message message = new TextMessage();
         message.setToUserName(command.getFromUserName());
         message.setFromUserName(command.getToUserName());
@@ -28,7 +35,7 @@ public class TextMsgListener extends Listener {
         if (command.getCommandKey().equals(TEXT_MSG)) {
             MpReply reply = mpReplyManager.getByKeyword(message.getContent());
             if (reply != null) {
-                message.setContent(command.getContent());
+                message.setContent(reply.getContent());
             } else {
                 message.setContent("");
             }
